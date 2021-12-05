7382f5fe432e0eda22fbd574abe6abd88b6b8d0e
Bishal Gautam <bishal.gautam@researchdisk.com>
2021-12-05 13:31:21 +0545

    project setup

---
+++ backend/src/main/java/com/fr/factorresearch/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.fr.factorresearch.repository;
+
+import com.fr.factorresearch.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ backend/src/main/java/com/fr/factorresearch/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.fr.factorresearch.web.rest.vm;
